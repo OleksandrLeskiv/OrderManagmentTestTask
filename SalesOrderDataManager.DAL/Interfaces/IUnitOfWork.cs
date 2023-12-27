c1a9d1fa9c0de49ba72f668d5e30db5707feb49d
OleksandrLeskiv <leskivolex@gmail.com>
2023-12-27 19:55:25 +0200

    Use pattern "UnitOfWork" to work with multiple repositories

---
+++ SalesOrderDataManager.DAL/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,9 @@
+﻿namespace SalesOrderDataManager.DAL.Interfaces;
+
+public interface IUnitOfWork
+{
+    public ISalesOrderRepository SalesOrders { get; }
+    public ISubElementRepository SubElements { get; }
+    public IWindowRepository Windows { get; }
+    public Task SaveAsync();
+}
---
+++ SalesOrderDataManager.DAL/UnitOfWork.cs
@@ -0,0 +1,29 @@
+﻿using SalesOrderDataManager.DAL.Context;
+using SalesOrderDataManager.DAL.Interfaces;
+
+namespace SalesOrderDataManager.DAL;
+
+public class UnitOfWork : IUnitOfWork
+{
+    private readonly ApplicationDbContext _context;
+    private readonly Lazy<ISalesOrderRepository> _salesOrders;
+    private readonly Lazy<ISubElementRepository> _subElements;
+    private readonly Lazy<IWindowRepository> _windows;
+
+    public UnitOfWork(ApplicationDbContext context, Lazy<ISalesOrderRepository> salesOrders, Lazy<ISubElementRepository> subElements,
+        Lazy<IWindowRepository> windows)
+    {
+        _context = context;
+        _salesOrders = salesOrders;
+        _subElements = subElements;
+        _windows = windows;
+    }
+
+    public ISalesOrderRepository SalesOrders => _salesOrders.Value;
+    public ISubElementRepository SubElements => _subElements.Value;
+    public IWindowRepository Windows => _windows.Value;
+    public async Task SaveAsync()
+    {
+        await _context.SaveChangesAsync();
+    }
+}
