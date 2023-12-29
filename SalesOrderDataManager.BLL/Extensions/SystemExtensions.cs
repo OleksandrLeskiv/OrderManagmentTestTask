152495acda1c819cbf6701e418312bfbdb309f11
OleksandrLeskiv <leskivolex@gmail.com>
2023-12-29 12:23:28 +0200

    add generic extension method to deep clone objects

---
+++ SalesOrderDataManager.BLL/Extensions/SystemExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+
+namespace SalesOrderDataManager.BLL.Extensions;
+
+public static class SystemExtensions
+{
+    public static T? Clone<T>(this T source)
+    {
+        var serialized = JsonConvert.SerializeObject(source);
+        return JsonConvert.DeserializeObject<T>(serialized);
+    }
+}
